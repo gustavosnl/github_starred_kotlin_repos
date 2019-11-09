55209b03696a607167e5ad5f408c8535bddae882
Gustavo Lima <gustavosnl@gmail.com>
2019-11-09 17:01:09 -0300

    criação do adapter e viewholder de listagem de repositórios

--- presentation/src/main/java/com/glima/github/repository/list/GithubRepositoriesAdapter.kt
+++ presentation/src/main/java/com/glima/github/repository/list/GithubRepositoriesAdapter.kt
@@ -1,4 +1,38 @@
 package com.glima.github.repository.list
 
-class GithubRepositoriesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.glima.github.R
+import com.glima.github.domain.business.model.Repository
+import com.glima.github.repository.list.GithubRepositoriesAdapter.GithubRepositoryViewHolder
+
+class GithubRepositoriesAdapter : RecyclerView.Adapter<GithubRepositoryViewHolder>() {
+
+    private val repositories = mutableListOf<Repository>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GithubRepositoryViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_repository, parent, false)
+        return GithubRepositoryViewHolder(view)
+    }
+
+    override fun getItemCount() = repositories.size
+
+    override fun onBindViewHolder(holder: GithubRepositoryViewHolder, position: Int) {
+        holder.bind(repositories[position])
+    }
+
+    fun addRepositories(list: List<Repository>) {
+        repositories.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    inner class GithubRepositoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(repository: Repository) {
+
+        }
+
+    }
 }
