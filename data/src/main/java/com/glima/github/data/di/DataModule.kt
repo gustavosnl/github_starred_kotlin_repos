5436c6ba6f3ce6d4c03181c3e30f886f8b65e411
Gustavo Lima <gustavosnl@gmail.com>
2019-11-09 14:20:43 -0300

    provisionando dependências para os repositórios e casos de uso

--- data/src/main/java/com/glima/github/data/di/DataModule.kt
+++ data/src/main/java/com/glima/github/data/di/DataModule.kt
@@ -1,8 +1,14 @@
 package com.glima.github.data.di
 
+import com.glima.github.data.repository.GithubRepositoryRepositoryImpl
+import com.glima.github.domain.repository.GithubRepositoryRepository
 import org.koin.dsl.module
 
 object DataModule {
 
-    val module = module { }
+    val module = module {
+        single<GithubRepositoryRepository> {
+            GithubRepositoryRepositoryImpl(api = get())
+        }
+    }
 }
--- domain/src/main/java/com/glima/github/domain/di/DomainModule.kt
+++ domain/src/main/java/com/glima/github/domain/di/DomainModule.kt
@@ -1,8 +1,15 @@
 package com.glima.github.domain.di
 
+import com.glima.github.domain.business.usecase.SeachKotlinRepositoriesUseCaseImpl
+import com.glima.github.domain.business.usecase.SearchRepositoriesUseCase
 import org.koin.dsl.module
 
 object DomainModule {
 
-    val module = org.koin.dsl.module {  }
+    val module = module {
+
+        single<SearchRepositoriesUseCase> {
+            SeachKotlinRepositoriesUseCaseImpl(repository = get())
+        }
+    }
 }
